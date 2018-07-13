e65c4b03ffe35aee83bc353dbd2e4676021c4ba5
eliahrebstock <eliah.rebstock@people-doc.com>
2018-07-13 14:13:14 +0200

    Divide tests in multiple methods

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,9 +1,9 @@
 import testinfra
 
+localhost = testinfra.get_host('local://')
+
 
 def test_containers():
-
-    localhost = testinfra.get_host('local://')
 
     # Check container exists
     localhost.check_output('docker container inspect container1-test')
@@ -15,12 +15,16 @@ def test_containers():
     # Check network has been created
     localhost.check_output('docker network inspect ansible-boot-docker-net')
 
+
+def test_port_bindings():
     cmd = localhost.run(
         "docker inspect -f '{{.HostConfig.PortBindings}}' container1-test"
     )
 
     assert 'map[80/tcp:[{0.0.0.0 8080}]]' in cmd.stdout
 
+
+def test_volumes_binds():
     cmd = localhost.run(
         "docker inspect -f '{{.HostConfig.Binds}}' container1-test"
     )
